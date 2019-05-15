6eb55d7b879aec2a0ab1fe7ef53ad3ba771cf4fe
Jason Calixto <JasonCalixto97@gmail.com>
2019-05-15 16:29:19 -0400

    GOT THE PICTURE TO SHOW!! WITH THE BORDER!!

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 class Classic {
   constructor() {
+    this.frame = "black-border"
+    this.art_url = "https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
 
   }
+
+  render() {
+    let img = document.createElement('img')
+    img.setAttribute('src', `${this.art_url}`)
+    img.setAttribute('border', `1px ${this.frame}`)
+    // img.setAttribute('border', this)
+    document.body.appendChild(img)
+  }
 }
+
+mona = new Classic()
+
+mona.render()

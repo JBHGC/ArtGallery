ef27845a52b4fe9978b9b5f2433f1b1405e4a0e9
Jason Calixto <JasonCalixto97@gmail.com>
2019-05-15 16:39:42 -0400

    Part 1 is done

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 class Classic {
-  constructor() {
+  constructor(art_url) {
     this.frame = "black-border"
-    this.art_url = "https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
-
+    if (art_url !== "https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg") {
+      this.art_url = art_url
+    } else {
+      this.art_url = "https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
+      }
   }
-
   render() {
     let img = document.createElement('img')
     img.setAttribute('src', `${this.art_url}`)
     img.setAttribute('border', `1px ${this.frame}`)
-    // img.setAttribute('border', this)
+    img.setAttribute('width', '10%')
     document.body.appendChild(img)
   }
 }
 
-mona = new Classic()
+class Memes {
+  constructor() {
+    
+  }
+}
 
-mona.render()
+
+var art1 = new Classic('http://simpleabstract.com/assets/images/Oil-On-Canvas-Abstract-Art.jpg')
+art1.render()
+// mona = new Classic()
+//  ALL TESTING STUFF
+// mona.render()
